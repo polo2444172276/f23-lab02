728a2a2e6c08ccdf69c0ea0bbd5b515a49880a62
Guolun Li <38054997+polo2444172276@users.noreply.github.com>
2023-09-07 19:56:55 -0400

    expanded Renderer from Rectangle only to Shap in general

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,17 +3,16 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    public Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = shape.getArea();
 
         // assume implementation
-
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
 }
